1913221d3f6f8eb13b452af63dfedfc2301bde23
Harrison Malone <harrison.malone2@gmail.com>
2019-07-03 14:17:13 +1000

    basic setup

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ require('dotenv').config()
 const app = new express();
 const port = process.env.PORT || 5000
 
+app.get('/hello', (req, res) => {
+  res.send({
+    hello: 'hello'
+  })
+})
+
 app.listen(port, () => {
   console.log(`running on port ${port}`)
 })
