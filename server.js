240407d3f6990f9fa503784ce74758e9068d4f6e
Harrison Malone <harrison.malone2@gmail.com>
2019-07-03 14:51:50 +1000

    think its done

--- server.js
+++ server.js
@@ -2,12 +2,35 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config()
 
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, (err) => {
+  if (err) {
+    console.log('not connected to db')
+  } else {
+    console.log('everything is working ✅')
+  }
+})
+
 const app = new express();
 const port = process.env.PORT || 5000
+const User = require('./models/User')
+
+app.use(express.json())
 
 app.get('/hello', (req, res) => {
   res.send({
     hello: 'hello'
+  })
+})
+
+app.post('/create-user', async (req, res) => {
+  const { username, password } = req.body
+  const newUser = new User({
+    username: username,
+    password: password
+  })
+  const savedUser = await newUser.save()
+  res.send({
+    savedUser: savedUser
   })
 })
 
